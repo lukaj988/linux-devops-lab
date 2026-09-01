4f952a0cb13f0cf3f1fe64e5cb609a86b50bca70
Luka Jovanovic <lukaj988@gmail.com>
2026-09-01 10:47:57 +0000

    Dodata GitHub Action

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "==========="
+echo "Status sistema"
+echo "==========="
+
+date
+
+echo ""
+echo "Memorija"
+free -h
+
+echo ""
+echo "Disk:"
+df -h
---
+++ docker-status.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Docker kontejner"
+
+docker ps
+
+echo ""
+
+docker images
+
